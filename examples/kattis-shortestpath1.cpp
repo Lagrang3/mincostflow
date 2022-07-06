d11bb6ba6f3469b8d1908cbe746884229f13144f
Lagrang3 <eduardo.quintana@pm.me>
2022-07-06 07:38:12 +0200

    kattis shortestpath1 example

--- examples/kattis-shortestpath1.cpp
+++ examples/kattis-shortestpath1.cpp
@@ -1,42 +1,60 @@
 // https://open.kattis.com/problems/shortestpath1
 
 #include "mincostflow/graph.hpp"
-#include "mincostflow/paths.hpp"
+#include "mincostflow/shortestpath.hpp"
 #include <iostream>
-// runtime 0.18s
+
+using length_type = int;
+typedef ln::shortestPath_Dijkstra<length_type> pathsolver_t; // 0.28s
+// typedef ln::shortestPath_FIFO<length_type> pathsolver_t; // 1.14s
+// typedef ln::shortestPath_BellmanFord<length_type> pathsolver_t; > // 3.0s
 
 int main()
 {
+
     while(1)
     {
         int N_vertex,N_edges,Q,S;
         std::cin>>N_vertex>>N_edges>>Q>>S;
         if(N_vertex==0)break;
         
-        ln::digraph Graph(N_vertex);
-        std::vector<int> weights(N_edges);
+        ln::digraph<int,int> Graph;
+        std::vector<length_type> weights(N_edges);
+        pathsolver_t solver;
         
         
         for(int e=0;e<N_edges;++e)
         {
             int a,b,w;
             std::cin>> a>>b>>w;
-            Graph.add_edge(a,b);
-            weights.at(e) = w;
+            auto [arc,arc2] = Graph.add_arc(a,b,e);
+            
+            if(weights.size()<Graph.max_num_arcs())
+                weights.resize(Graph.max_num_arcs());
+                
+            weights.at(arc.x) = w;
+            weights.at(arc2.x) = solver.INFINITY;
         }
         
-        ln::shortest_path_dijkstra solver(Graph);
-        solver(S,weights,[](int e){return true;});
+        using arc_pos_t = typename ln::digraph_types::arc_pos_t;
+        // using node_pos_t = typename ln::digraph_types::node_pos_t;
+        
+        auto s_node = Graph.add_node(S);
+        solver.solve(Graph,s_node,weights,
+            [&weights,&solver](arc_pos_t arc){return weights.at(arc.x)<solver.INFINITY;});
+        
         
         for(int v;Q--;)
         {
             std::cin>>v; 
-            if(!solver.is_connected(v))
-                std::cout << "Impossible\n";
+            auto pos = Graph.get_node(v);
+            if(Graph.is_valid(pos) && solver.distance.at(pos.x)<solver.INFINITY)
+            {
+                std::cout << solver.distance.at(pos.x) << '\n';
+            }
             else
             {
-                auto d = solver.get_distance(v); 
-                std::cout << d << '\n';
+                std::cout << "Impossible\n";
             }
         }
         
